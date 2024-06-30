966ed30f536c1ec9039a28d5202b1072f2c5d447
Caoimhín Ó Donnaíle <caoimhin@smo.uhi.ac.uk>
2024-06-30 13:57:55 +0000

    Ann an f.php agus d.php, ceartaich bug a thaobh topar nach eil (fhathast) anns an table topar

--- f.php
+++ f.php
@@ -351,7 +351,8 @@ END_fiosTableHtml;
         if (!empty($dictHtml)) { $dictHtml = "<fieldset class=imrad style='background-color:#fee'>\n<legend title='$T_Faclairean_fios'>$T_Faclairean</legend>\n<ul>\n$dictHtml\n</ul>\n</fieldset>\n"; }
     }
 
-    $stmtd1 = $DbBun->prepare('SELECT bundf.d, topar.topar, topar.ainm, ABS(meit) AS meit0, ciana AS ciana0 FROM bundf, bund LEFT JOIN topar ON bund.topar=topar.topar'
+    $stmtd1 = $DbBun->prepare('SELECT bundf.d, bund.topar, topar.ainm AS ainmTopar, ABS(meit) AS meit0, ciana AS ciana0'
+                            . ' FROM bundf, bund LEFT JOIN topar ON bund.topar=topar.topar'
                             . ' WHERE f=:f AND bundf.d=bund.d ORDER BY meit0,ciana0,d');
     $queryd2 = 'SELECT bunf.f AS f2, bunf.t AS t2, bunf.focal AS focal2, bunf.focal_ci AS focal2_ci, bunf.derb AS derb2, meit, ciana, doich, parentage_ord'
              . ' FROM bundf,bunf,bunt'
@@ -361,6 +362,7 @@ END_fiosTableHtml;
              . ' ORDER BY ciana,meit,parentage_ord,focal2_ci,focal2,derb2';
     $stmtd2 = $DbBun->prepare($queryd2);
     $stmtd1->execute(array(':f'=>$f));
+    $ainmTopar = $ainmTopar ?? '';
     while ($row1 = $stmtd1->fetch(PDO::FETCH_ASSOC)) {
         extract($row1);
         $dDeasaichHtml = ( $deasaich
@@ -441,7 +443,7 @@ END_fiosTableHtml;
         }
         $dronganHtml .= <<<END_drongHtml
 <div class='drong$dClass' id='d$d'>
-<div class=dCeann><b><a href='d.php?d=$d'>$T_Drong $d</a></b>$dDeasaichHtml &nbsp; <span class=topar title='$T_topar: $ainm'>($topar)</span> $bearrBarrHtml</div>
+<div class=dCeann><b><a href='d.php?d=$d'>$T_Drong $d</a></b>$dDeasaichHtml &nbsp; <span class=topar title='$T_topar: $ainmTopar'>($topar)</span> $bearrBarrHtml</div>
 <div class=dColainn>
 <table>
 $dTableHtml
